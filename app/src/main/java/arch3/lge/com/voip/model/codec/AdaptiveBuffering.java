e0d8eae39a994de71a4ad4b0b25a57bd351f812b
junam.pak <junam.pak@lge.com>
2019-06-28 14:18:33 -0400

    Capacity update

--- app/src/main/java/arch3/lge/com/voip/model/codec/AdaptiveBuffering.java
+++ app/src/main/java/arch3/lge/com/voip/model/codec/AdaptiveBuffering.java
@@ -12,6 +12,8 @@ public class AdaptiveBuffering {
     private int mSequenceNumber = 0;
     private LinkedBlockingQueue<byte[]> mPacketQueue = new LinkedBlockingQueue<>(1024);
     private int mQueueCapacity = 16;
+    private int mLastSequence = 0;
+    private byte [] mLastPacket;
 
     private final static long COEF_A = 500;
     private final static long COEF_B = 4;
@@ -29,20 +31,37 @@ public class AdaptiveBuffering {
 
     int readHeader(byte [] data){
         ByteBuffer byteBuffer = ByteBuffer.wrap(data, 0, HDR_SIZE);
+
         int index = byteBuffer.getInt();
+        if(index == mLastSequence + 2){
+            Log.d(LOG_TAG, "packet missing  ="+ (index-1));
+            if(mLastPacket != null) {
+                Log.d(LOG_TAG, "repleace to  last packet");
+                mPacketQueue.add(mLastPacket);
+                mLastPacket = null;
+            }
+        }
+        mLastSequence = index;
+
         long delay = System.currentTimeMillis() - byteBuffer.getLong();
-        mQueueCapacity = calcBufferSize(index, delay);
+        calcBufferSize(index, delay);
         return HDR_SIZE;
     }
 
-    private int calcBufferSize(int index, long delay){
+    private void calcBufferSize(int index, long delay){
         Log.d(LOG_TAG, "indx ="+ index+  " delay ="+ delay);
         mAveDi = (mAveDi*(COEF_A-1) + delay) / COEF_A;
         mAveVi = (mAveVi*(COEF_A-1) + Math.abs(mAveDi - delay)) / COEF_A;
-        return (int)(mAveDi + COEF_B*mAveVi)/10;
+        long Pi = mAveDi + COEF_B*mAveVi;
+        Log.d(LOG_TAG, "Di ="+ mAveDi+  " Vi ="+ mAveVi + " => "+ Pi);
+        if((mLastSequence % 1000) == 0){
+         //   mQueueCapacity = (int)Pi / 10;
+            Log.d(LOG_TAG, "Capacity  update ="+ mQueueCapacity);
+        }
     }
 
     void addQueue(byte [] data){
+        mLastPacket = data;
         mPacketQueue.add(data);
         if(mPacketQueue.size( )> mQueueCapacity){
             mPacketQueue.remove();
